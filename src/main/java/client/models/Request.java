b43a288cb037d2d0674c48eae89ad7d0fad6c98f
mohammad hosain jaafari <dsazxasqw01@gmail.com>
2024-06-09 14:31:39 +0330

    [added] most of the requests created [update] a little code comment :)

--- src/main/java/client/models/Request.java
+++ src/main/java/client/models/Request.java
@@ -6,6 +6,7 @@ import org.json.simple.JSONObject;
 import static client.models.Main.write;
 
 public class Request {
+    //account requests--------------------------------------------------------------------------------------
 
     public void login(String username_input,String password_input){
         JSONObject request = new JSONObject();
@@ -15,7 +16,6 @@ public class Request {
 
         write(request);
     }
-
     public void signUp(String username_input,String password_input,String name_input,String email_input,String number_input){
         JSONObject request = new JSONObject();
         request.put("requestType","/signUp");
@@ -25,9 +25,27 @@ public class Request {
         request.put("email_input",email_input);
         request.put("number_input",number_input);
 
+        write(request);
+    }
+    public void logout(String username_input,String password_input){
+        JSONObject request = new JSONObject();
+        request.put("requestType","/logout");
+        request.put("username",username_input);
+        request.put("password",password_input);
 
         write(request);
     }
+    public void authorize(String username, String password){
+        JSONObject request = new JSONObject();
+        request.put("requestType","/authorize");
+        request.put("username",username);
+        request.put("password",password);
+
+        write(request);
+    }
+    //-------------------------------------------------------------------------------------------------------
+
+    // item list request-------------------------------------------------------------------------------------
     static void videoList(String videoCount, JSONArray tags){
         JSONObject request = new JSONObject();
         request.put("requestType","/videoList");
@@ -36,7 +54,26 @@ public class Request {
 
         write(request);
     }
+    static void channelList(String channelCount, JSONArray tags){
+        JSONObject request = new JSONObject();
+        request.put("requestType","/channelList");
+        request.put("count",channelCount);
+        request.put("tags",tags);
 
+        write(request);
+    }
+    static void hybridList(String itemCount, JSONArray tags){
+        //channel and video list combined
+        JSONObject request = new JSONObject();
+        request.put("requestType","/hybridList");
+        request.put("count",itemCount);
+        request.put("tags",tags);
+
+        write(request);
+    }
+    //-------------------------------------------------------------------------------------------------------
+
+    // item request------------------------------------------------------------------------------------------
     static void video(String video_id){
         JSONObject request = new JSONObject();
         request.put("requestType","/video");
@@ -59,6 +96,155 @@ public class Request {
 
         write(request);
     }
-    //test7
+    public void channel(String channel_id){
+        JSONObject request = new JSONObject();
+        request.put("requestType","/channel");
+        request.put("channel_id",channel_id);
+
+
+        write(request);
+    }
+    //------------------------------------------------------------------------------------------------------
+
+    // submit requests -------------------------------------------------------------------------------------
+    public void createChannel(String channelName,JSONArray tags){
+
+        JSONObject request = new JSONObject();
+        request.put("requestType","/createChannel");
+        request.put("channelName",channelName);
+        request.put("tags",tags);
+
+
+        write(request);
+    }
+    public void createVideo(String channel_id,String videoName,String videoCaption,JSONArray tags){
+
+        JSONObject request = new JSONObject();
+        request.put("requestType","/createVideo");
+        request.put("channel_id",channel_id);
+        request.put("videoName",videoName);
+        request.put("videoCaption",videoCaption);
+        request.put("tags",tags);
+
+
+        write(request);
+    }
+    public void subscribeChannel(String channel_id, String videoName, String videoCaption, JSONArray tags){
+
+        JSONObject request = new JSONObject();
+        request.put("requestType","/subscribeChannel");
+        request.put("channel_id",channel_id);
+
+
+        write(request);
+    }
+    public void unsubscribeChannel(String channel_id, String videoName, String videoCaption, JSONArray tags){
+
+        JSONObject request = new JSONObject();
+        request.put("requestType","/unsubscribeChannel");
+        request.put("channel_id",channel_id);
+
+
+
+        write(request);
+    }
+    //------------------------------------------------------------------------------------------------------
+
+    // tiny action request----------------------------------------------------------------------------------
+    static void add_WatchedVideo(String video_id){
+        JSONObject request = new JSONObject();
+        request.put("requestType","/watchVideo");
+        request.put("video_id",video_id);
+
+        write(request);
+    }
+    static void remove_WatchedVideo(String video_id){
+        JSONObject request = new JSONObject();
+        request.put("requestType","/remove_WatchedVideo");
+        request.put("video_id",video_id);
+
+
+        write(request);
+    }
+    static void add_WatchLaterVideo(String video_id){
+        JSONObject request = new JSONObject();
+        request.put("requestType","/add_WatchLaterVideo");
+        request.put("video_id",video_id);
+
+        write(request);
+    }
+    static void remove_WatchLaterVideo(String video_id){
+        JSONObject request = new JSONObject();
+        request.put("requestType","/remove_WatchLaterVideo");
+        request.put("video_id",video_id);
+
+
+        write(request);
+    }
+    static void add_likedVideo(String video_id){
+        JSONObject request = new JSONObject();
+        request.put("requestType","/add_likedVideo");
+        request.put("video_id",video_id);
+
+
+        write(request);
+    }
+    static void remove_likedVideo(String video_id){
+        JSONObject request = new JSONObject();
+        request.put("requestType","/remove_likedVideo");
+        request.put("video_id",video_id);
+
+
+        write(request);
+    }
+    static void add_dislikedVideo(String video_id){
+        JSONObject request = new JSONObject();
+        request.put("requestType","/add_dislikedVideo");
+        request.put("video_id",video_id);
+
+        write(request);
+    }
+    static void remove_dislikedVideo(String video_id){
+        JSONObject request = new JSONObject();
+        request.put("requestType","/remove_dislikedVideo");
+        request.put("video_id",video_id);
+
+        write(request);
+    }
+    static void add_comment(String comment_id,String repliedTo_id){
+        JSONObject request = new JSONObject();
+        request.put("requestType","/add_comment");
+        request.put("comment_id",comment_id);
+        request.put("repliedTo_id",repliedTo_id);
+
+        write(request);
+    }
+    static void remove_comment(String comment_id,String repliedTo_id){
+        JSONObject request = new JSONObject();
+        request.put("requestType","/remove_comment");
+        request.put("comment_id",comment_id);
+        request.put("repliedTo_id",repliedTo_id);
+
+        write(request);
+    }
+    static void add_commentLike(String comment_id,String repliedTo_id){
+        JSONObject request = new JSONObject();
+        request.put("requestType","/add_commentLike");
+        request.put("comment_id",comment_id);
+        request.put("repliedTo_id",repliedTo_id);
+
+
+        write(request);
+    }
+    static void remove_commentLike(String comment_id,String repliedTo_id){
+        JSONObject request = new JSONObject();
+        request.put("requestType","/remove_commentLike");
+        request.put("comment_id",comment_id);
+        request.put("repliedTo_id",repliedTo_id);
+
+
+        write(request);
+    }
+    //-----------------------------------------------------------------------------------------------------
 
 }
