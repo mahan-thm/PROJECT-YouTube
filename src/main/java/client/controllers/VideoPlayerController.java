5c98825ef6a100e8049379407045b799c1ea629a
moon <m.tahmasbi1383@gmail.com>
2024-06-12 05:42:32 +0330

    [update] VideoPlayer.fxml, VideoPlayerController

--- src/main/java/client/controllers/VideoPlayerController.java
+++ src/main/java/client/controllers/VideoPlayerController.java
@@ -1,13 +1,22 @@
 package client.controllers;
 
+import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.util.Duration;
 
+import java.io.File;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class VideoPlayerController implements Initializable {
@@ -19,20 +28,66 @@ public class VideoPlayerController implements Initializable {
     private VBox videoComments_vBox;
     @FXML
     private VBox recommendVideo_vBox;
+    @FXML
+    private MediaView video_mediaView;
+    @FXML
+    private Slider videoTimline_slider;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        for(int i = 0; i < 60; i++){
-            Label label = new Label("Hi");
-            videoComments_vBox.getChildren().add(label);
+
+
+        File file = new File(Objects.requireNonNull(getClass().getResource("../../VideoPlayer/VideoTest.mp4")).getFile());
+        Media media = new Media(file.toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        video_mediaView.setMediaPlayer(mediaPlayer);
+
+        mediaPlayer.totalDurationProperty().addListener((obs, oldDuration, newDuration) ->
+                videoTimline_slider.setMax(newDuration.toSeconds())
+        );
+
+        mediaPlayer.currentTimeProperty().addListener((obs, oldTime, newTime) -> {
+            if (!videoTimline_slider.isValueChanging()) {
+                videoTimline_slider.setValue(newTime.toSeconds());
+            }
+        });
+
+
+//        videoTimline_slider.valueChangingProperty().addListener((obs, wasChanging, isNowChanging) -> {
+//                if (!isNowChanging) {
+//                    mediaPlayer.seek(Duration.seconds(videoTimline_slider.getValue()));
+//                }
+//            });
+
+
+        videoTimline_slider.addEventFilter(MouseEvent.MOUSE_DRAGGED, e -> {
+            mediaPlayer.seek(Duration.seconds(videoTimline_slider.getValue()));
+        });
+
+        //lag
+//        videoTimline_slider.valueProperty().addListener((obs, oldValue, newValue) -> {
+//            if (!videoTimline_slider.isValueChanging()) {
+//                mediaPlayer.seek(Duration.seconds(newValue.doubleValue()));
+//            }
+//        });
+
+        videoTimline_slider.setOnMouseDragExited((MouseEvent mouseEvent) -> {
+            mediaPlayer.seek(Duration.seconds(videoTimline_slider.getValue()));
+        });
+
+            mediaPlayer.play();
+
+            for (int i = 0; i < 60; i++) {
+                Label label = new Label("Hi");
+                videoComments_vBox.getChildren().add(label);
+            }
         }
+
+        public void toolBar_action () {
+            boolean toolbarShow = toolBar_pane.isVisible();
+
+            toolBar_pane.setVisible(!toolbarShow);
+            toolBar_vBox.setVisible(toolbarShow);
+        }
+
     }
-
-    public void toolBar_action() {
-        Boolean toolbarShow = toolBar_pane.isVisible();
-
-        toolBar_pane.setVisible(!toolbarShow);
-        toolBar_vBox.setVisible(toolbarShow);
-    }
-
-}
