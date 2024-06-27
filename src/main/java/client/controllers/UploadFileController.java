db9f7e7574b6b89f0b2b290973a7924dd299483e
moon <m.tahmasbi1383@gmail.com>
2024-06-27 23:31:42 +0330

    [update] UploadFile.fxml > select file done

--- src/main/java/client/controllers/UploadFileController.java
+++ src/main/java/client/controllers/UploadFileController.java
@@ -1,4 +1,70 @@
 package client.controllers;
 
-public class UploadFileController {
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+
+public class UploadFileController implements Initializable {
+
+    private String selectedPath;
+
+    @FXML
+    private ImageView upload_imageView;
+    @FXML
+    private Label selectedFile_label;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
+    public void selectFile_action(ActionEvent actionEvent) {
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select a File");
+        FileChooser.ExtensionFilter mp4Filter = new FileChooser.ExtensionFilter("MP4 Files", "*.mp4");
+        fileChooser.getExtensionFilters().addAll(mp4Filter);
+        File selectedFile = fileChooser.showOpenDialog(stage);
+
+        selectedPath = selectedFile.getAbsolutePath();
+        selectedFile_label.setText("SELECTED FILE:\n" + selectedPath);
+
+        try {
+            Image uploadGif = new Image(Objects.requireNonNull(getClass().getResource("../../creat/uploadGif.gif")).openStream());
+            upload_imageView.setImage(uploadGif);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Timeline delayTimeline = new Timeline(
+                new KeyFrame(Duration.seconds(2.766667), event ->
+                        upload_imageView.setVisible(false))
+        );
+        delayTimeline.play();
+
+    }
+
+    public void close_action(ActionEvent actionEvent) {
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
 }
--- src/main/java/client/models/TestMain.java
+++ src/main/java/client/models/TestMain.java
@@ -5,7 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -20,17 +22,22 @@ public class TestMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 //        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../../entry/Login.fxml")));
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../../home/Home.fxml")));
+//        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../../home/Home.fxml")));
 //        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../../videoPlayer/videoPlayer.fxml")));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../../creat/UploadFile.fxml")));
 
         Scene scene = new Scene(root);
 
 //        String css = Objects.requireNonNull(this.getClass().getResource("../../entry/LoginStyle.css")).toExternalForm();
-        String css = Objects.requireNonNull(this.getClass().getResource("../../home/HomeStyle.css")).toExternalForm();
-//        String css = Objects.requireNonNull(this.getClass().getResource("../../videoPlayer/VideoPlayerStyle.css")).toExternalForm();
+        String css = Objects.requireNonNull(this.getClass().getResource("../../creat/UploadFileStyle.css")).toExternalForm();
+////        String css = Objects.requireNonNull(this.getClass().getResource("../../videoPlayer/VideoPlayerStyle.css")).toExternalForm();
         scene.getStylesheets().add(css);
+scene.setFill(Color.TRANSPARENT);
+
+        stage.initStyle(StageStyle.TRANSPARENT);
 
         stage.setTitle("YouTube");
+
         stage.setScene(scene);
 
         Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("../../entry/youtube-icon.png")));
