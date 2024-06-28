31e04bbe860c34bacdde248d465d2b6f93e03bc7
moon <m.tahmasbi1383@gmail.com>
2024-06-28 06:48:54 +0330

    [update] UploadFile.fxml > transitions

--- src/main/java/client/controllers/UploadFileController.java
+++ src/main/java/client/controllers/UploadFileController.java
@@ -1,16 +1,21 @@
 package client.controllers;
 
+import javafx.animation.FadeTransition;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.skin.ScrollBarSkin;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -25,17 +30,26 @@ import java.util.ResourceBundle;
 public class UploadFileController implements Initializable {
 
     private String selectedPath;
-
+@FXML
+private AnchorPane uploadVideo_anchorPain;
     @FXML
     private ImageView upload_imageView;
     @FXML
     private Label selectedFile_label;
     @FXML
     private ScrollPane details_scrollPane;
+    @FXML
+    private Pane selectFile_pane;
+    @FXML
+    private Pane details_pane;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(1.5));
+        fadeTransition.setNode(uploadVideo_anchorPain);
+        fadeTransition.setFromValue(0);
+        fadeTransition.setToValue(1);
+        fadeTransition.play();
     }
 
 
@@ -69,9 +83,38 @@ public class UploadFileController implements Initializable {
 
     }
 
+    @FXML
     public void close_action(ActionEvent actionEvent) {
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.5));
+        fadeTransition.setNode(uploadVideo_anchorPain);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+        fadeTransition.setOnFinished(actionEvent1 -> {
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         stage.close();
+        });
+        fadeTransition.play();
     }
 
+    @FXML
+    public void next_action() {
+        selectFile_pane.setVisible(true);
+        details_pane.setVisible(true);
+        TranslateTransition translateTransition1 = new TranslateTransition(Duration.seconds(1.5));
+        translateTransition1.setNode(selectFile_pane);
+        translateTransition1.setToX(-750);
+
+
+        details_pane.setLayoutX(750);
+        TranslateTransition translateTransition2 = new TranslateTransition(Duration.seconds(1.5));
+        translateTransition2.setNode(details_pane);
+        translateTransition2.setToX(-750);
+        translateTransition1.setOnFinished(actionEvent -> {
+                    selectFile_pane.setVisible(false);
+                }
+        );
+
+        translateTransition1.play();
+        translateTransition2.play();
+    }
 }
