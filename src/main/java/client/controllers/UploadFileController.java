5f113200fbeb86d286579908dea5f0c5fa949742
moon <m.tahmasbi1383@gmail.com>
2024-07-02 04:41:55 +0330

    [update] UploadFile.fxml

--- src/main/java/client/controllers/UploadFileController.java
+++ src/main/java/client/controllers/UploadFileController.java
@@ -8,17 +8,17 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.skin.ScrollBarSkin;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import javafx.util.Duration;
 
 import java.io.File;
@@ -30,9 +30,10 @@ import java.util.ResourceBundle;
 
 public class UploadFileController implements Initializable {
 
-    private String selectedPath;
-@FXML
-private AnchorPane uploadVideo_anchorPain;
+    private String filePath;
+    private boolean childAbuse;
+    @FXML
+    private AnchorPane uploadVideo_anchorPain;
     @FXML
     private ImageView upload_imageView;
     @FXML
@@ -43,6 +44,18 @@ private AnchorPane uploadVideo_anchorPain;
     private Pane selectFile_pane;
     @FXML
     private Pane details_pane;
+    @FXML
+    private Label videoPath_label;
+    @FXML
+    private RadioButton childYes_radioButton;
+    @FXML
+    private RadioButton childNo_radioButton;
+    @FXML
+    private RadioButton private_RadiButton;
+    @FXML
+    private RadioButton public_radioButton;
+    @FXML
+    private MediaView video_mediaView;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -66,9 +79,26 @@ private AnchorPane uploadVideo_anchorPain;
 
         scaleTransition.play();
         fadeTransition.play();
+        //________________________________________
+        if (childYes_radioButton.isArmed()) {
+            childNo_radioButton.disarm();
+        } else if (childNo_radioButton.isArmed()) {
+            childNo_radioButton.disarm();
+        }
+
+        if (public_radioButton.isArmed()) {
+            private_RadiButton.disarm();
+        } else if (private_RadiButton.isArmed()) {
+            public_radioButton.disarm();
+        }
+
+        //_________________________________________
+        //TODO
+        //here you should set the name of play lists to this combo box
+//        selectPlaylist_comboBox.setButtonCell();
     }
 
-
+    @FXML
     public void selectFile_action(ActionEvent actionEvent) {
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         FileChooser fileChooser = new FileChooser();
@@ -77,8 +107,8 @@ private AnchorPane uploadVideo_anchorPain;
         fileChooser.getExtensionFilters().addAll(mp4Filter);
         File selectedFile = fileChooser.showOpenDialog(stage);
 
-        selectedPath = selectedFile.getAbsolutePath();
-        selectedFile_label.setText("SELECTED FILE:\n" + selectedPath);
+        filePath = selectedFile.getAbsolutePath();
+        selectedFile_label.setText("SELECTED FILE:\n" + filePath);
 
         try {
             Image uploadGif = new Image(Objects.requireNonNull(getClass().getResource("../../creat/uploadGif.gif")).openStream());
@@ -86,11 +116,7 @@ private AnchorPane uploadVideo_anchorPain;
 
             upload_imageView.setImage(uploadGif);
 
-            Timeline delayTimeline = new Timeline(
-                    new KeyFrame(Duration.seconds(2.566667), event ->
-                            upload_imageView.setImage(uploadImage2)
-                    )
-            );
+            Timeline delayTimeline = new Timeline(new KeyFrame(Duration.seconds(2.566667), event -> upload_imageView.setImage(uploadImage2)));
             delayTimeline.play();
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -106,8 +132,8 @@ private AnchorPane uploadVideo_anchorPain;
         fadeTransition.setFromValue(1);
         fadeTransition.setToValue(0);
         fadeTransition.setOnFinished(actionEvent1 -> {
-        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        stage.close();
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.close();
         });
 
         ScaleTransition scaleTransition = new ScaleTransition(Duration.seconds(1));
@@ -125,38 +151,71 @@ private AnchorPane uploadVideo_anchorPain;
     }
 
     @FXML
+    private TextField videoTitle_textField;
+    @FXML
+    private TextArea description_textArea;
+    @FXML
+    private ComboBox<String> selectPlaylist_comboBox;
+    @FXML
+    private TextArea addTag_textArea;
+
+    @FXML
     public void next_action() {
-        FadeTransition fadeTransition1 = new FadeTransition(Duration.seconds(0.5));
-        fadeTransition1.setNode(selectFile_pane);
-        fadeTransition1.setFromValue(1);
-        fadeTransition1.setToValue(0);
 
-        FadeTransition fadeTransition2 = new FadeTransition(Duration.seconds(1));
-        fadeTransition2.setNode(details_pane);
-        fadeTransition2.setFromValue(0);
-        fadeTransition2.setToValue(1);
-
-        selectFile_pane.setVisible(true);
-        details_pane.setVisible(true);
-        TranslateTransition translateTransition1 = new TranslateTransition(Duration.seconds(1));
-        translateTransition1.setNode(selectFile_pane);
-        translateTransition1.setToX(-750);
+//        String videoLength = null;
+//        String videoSize = null;
+        videoPath_label.setText("Video path: " + filePath);
 
 
-        details_pane.setLayoutX(750);
-        TranslateTransition translateTransition2 = new TranslateTransition(Duration.seconds(1));
-        translateTransition2.setNode(details_pane);
-        translateTransition2.setToX(-750);
-        translateTransition1.setOnFinished(actionEvent -> {
-                    selectFile_pane.setVisible(false);
-                }
-        );
-        fadeTransition1.play();
-        fadeTransition2.play();
-        translateTransition1.play();
-        translateTransition2.play();
+        //__________________________________________
+        File file = new File(filePath);
+        Media media = new Media(file.toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        video_mediaView.setMediaPlayer(mediaPlayer);
+        video_mediaView.fitWidthProperty().set(230);
+        video_mediaView.fitHeightProperty().set(129.4);
+        video_mediaView.minWidth(230);
+        video_mediaView.maxWidth(230);
+        video_mediaView.minHeight(129.4);
+        video_mediaView.maxHeight(129.4);
+        mediaPlayer.setMute(true);
+        mediaPlayer.play();
+
+        if (!Objects.equals(filePath, "")) {
+            FadeTransition fadeTransition1 = new FadeTransition(Duration.seconds(0.5));
+            fadeTransition1.setNode(selectFile_pane);
+            fadeTransition1.setFromValue(1);
+            fadeTransition1.setToValue(0);
+
+            FadeTransition fadeTransition2 = new FadeTransition(Duration.seconds(1));
+            fadeTransition2.setNode(details_pane);
+            fadeTransition2.setFromValue(0);
+            fadeTransition2.setToValue(1);
+
+            selectFile_pane.setVisible(true);
+            details_pane.setVisible(true);
+            TranslateTransition translateTransition1 = new TranslateTransition(Duration.seconds(1));
+            translateTransition1.setNode(selectFile_pane);
+            translateTransition1.setToX(-750);
+
+
+            details_pane.setLayoutX(750);
+            TranslateTransition translateTransition2 = new TranslateTransition(Duration.seconds(1));
+            translateTransition2.setNode(details_pane);
+            translateTransition2.setToX(-750);
+            translateTransition1.setOnFinished(actionEvent -> {
+                selectFile_pane.setVisible(false);
+            });
+            fadeTransition1.play();
+            fadeTransition2.play();
+            translateTransition1.play();
+            translateTransition2.play();
+        } else {
+            selectedFile_label.setText("You didn't select a file!");
+        }
     }
 
+    @FXML
     public void refresh_action(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../../creat/UploadFile.fxml")));
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
@@ -170,5 +229,108 @@ private AnchorPane uploadVideo_anchorPain;
         Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("../../creat/uploadImage.jpg")));
         stage.getIcons().add(icon);
         stage.show();
+    }
+
+    @FXML
+    private Label status_label;
+
+    @FXML
+    public void upload_action(ActionEvent actionEvent) {
+        boolean okayToUpload = true;
+        //TODO
+
+        String videoTitle = videoTitle_textField.getText();
+        if (videoTitle == null) {
+            status_label.setText("Video title required");
+//            okayToUpload = false;
+        }
+
+        String videoDescription = description_textArea.getText();
+        String videoTags = addTag_textArea.getText();
+
+        if (childNo_radioButton.isArmed()) {
+            //child abuse is on
+        } else if (childNo_radioButton.isArmed()) {
+            //child abuse is off
+        } else {
+            status_label.setText("Audience button didn't check");
+//            okayToUpload = false;
+        }
+
+        if(private_RadiButton.isArmed()){
+            //privet
+        } else if (public_radioButton.isArmed()) {
+            //public
+        }else {
+            status_label.setText("Visibility button didn't check");
+//            okayToUpload = false;
+        }
+
+
+        if (okayToUpload) {
+            //TODO
+            //ofter everything is okay, what should happen?
+
+
+
+            //________________________________
+            FadeTransition fadeTransition = new FadeTransition(Duration.seconds(1));
+            fadeTransition.setNode(uploadVideo_anchorPain);
+            fadeTransition.setFromValue(1);
+            fadeTransition.setToValue(0);
+            fadeTransition.setOnFinished(actionEvent1 -> {
+                Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                stage.close();
+            });
+
+            ScaleTransition scaleTransition = new ScaleTransition(Duration.seconds(1));
+            scaleTransition.setNode(uploadVideo_anchorPain);
+            scaleTransition.setCycleCount(1);
+            uploadVideo_anchorPain.setScaleX(1);
+            uploadVideo_anchorPain.setScaleY(1);
+            uploadVideo_anchorPain.setScaleZ(1);
+            scaleTransition.setToX(0.1);
+            scaleTransition.setToY(0.1);
+            scaleTransition.setToZ(0.1);
+
+            scaleTransition.play();
+            fadeTransition.play();
+        }
+
+
+    }
+
+    @FXML
+    public void back_action() {
+        selectFile_pane.setVisible(true);
+        details_pane.setVisible(true);
+
+        FadeTransition fadeTransition1 = new FadeTransition(Duration.seconds(1));
+        fadeTransition1.setNode(selectFile_pane);
+        fadeTransition1.setFromValue(0);
+        fadeTransition1.setToValue(1);
+
+        FadeTransition fadeTransition2 = new FadeTransition(Duration.seconds(0.5));
+        fadeTransition2.setNode(details_pane);
+        fadeTransition2.setFromValue(1);
+        fadeTransition2.setToValue(0);
+
+        selectFile_pane.setLayoutX(-750);
+        TranslateTransition translateTransition1 = new TranslateTransition(Duration.seconds(1));
+        translateTransition1.setNode(selectFile_pane);
+        translateTransition1.setToX(750);
+
+
+        details_pane.setLayoutX(750);
+        TranslateTransition translateTransition2 = new TranslateTransition(Duration.seconds(1));
+        translateTransition2.setNode(details_pane);
+        translateTransition2.setToX(750);
+        translateTransition1.setOnFinished(actionEvent -> {
+            details_pane.setVisible(false);
+        });
+        fadeTransition1.play();
+        fadeTransition2.play();
+        translateTransition1.play();
+        translateTransition2.play();
     }
 }
