9743ce7b82b961ab79dc16fbcad71bf8c6d128c0
moon <m.tahmasbi1383@gmail.com>
2024-07-07 13:35:54 +0330

    [update] VideoPlayer > fullScreen

--- src/main/java/client/controllers/VideoPlayerController.java
+++ src/main/java/client/controllers/VideoPlayerController.java
@@ -103,6 +103,8 @@ public class VideoPlayerController {
     private Button mute_button;
     @FXML
     private Label duration_label;
+    @FXML
+    private VBox video_vBox;
 
     public void setup() {
 
@@ -401,4 +403,31 @@ public class VideoPlayerController {
             mute_button.setStyle("-fx-shape: 'm 21.48,17.98 c 0,-1.77 -1.02,-3.29 -2.5,-4.03 v 2.21 l 2.45,2.45 c .03,-0.2 .05,-0.41 .05,-0.63 z m 2.5,0 c 0,.94 -0.2,1.82 -0.54,2.64 l 1.51,1.51 c .66,-1.24 1.03,-2.65 1.03,-4.15 0,-4.28 -2.99,-7.86 -7,-8.76 v 2.05 c 2.89,.86 5,3.54 5,6.71 z M 9.25,8.98 l -1.27,1.26 4.72,4.73 H 7.98 v 6 H 11.98 l 5,5 v -6.73 l 4.25,4.25 c -0.67,.52 -1.42,.93 -2.25,1.18 v 2.06 c 1.38,-0.31 2.63,-0.95 3.69,-1.81 l 2.04,2.05 1.27,-1.27 -9,-9 -7.72,-7.72 z m 7.72,.99 -2.09,2.08 2.09,2.09 V 9.98 z'");
         }
     }
+
+    private int i = 1;
+
+    @FXML
+    public void speed_action() {
+        if (i == 0) {
+            mediaPlayer.setRate(1);
+            i = (i + 1) % 3;
+        } else if (i == 1) {
+            mediaPlayer.setRate(2);
+            i = (i + 1) % 3;
+        } else if (i == 2) {
+            mediaPlayer.setRate(0.5);
+            i = (i + 1) % 3;
+        }
+    }
+
+    @FXML
+    public void fullScreen_action(ActionEvent actionEvent) {
+        Pane pane = new Pane(video_vBox);
+        Scene scene = new Scene(pane);
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setFullScreen(true);
+        stage.show();
+    }
+
 }
